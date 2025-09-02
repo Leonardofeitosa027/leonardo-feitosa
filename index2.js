f7edc1715040ace4dfcf9aada6e49460ac10a654
Leonardofeitosa027 <feitosa.silva.leonardo@escola.pr.gov.br>
2025-09-02 14:50:10 -0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,26 @@
+.dropdown-conteudo {
+    display: none;  /* Inicialmente escondido */
+    background-color: #f9f9f9;
+    border: 1px solid #ddd;
+    padding: 10px;
+    margin-top: 5px;
+  }
+  
+  .dropdown-conteudo select {
+    width: 100%;
+    padding: 8px;
+    border-radius: 4px;
+  }
+  
+  .fechar {
+    color: red;
+    cursor: pointer;
+    font-size: 18px;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+  
+  .fechar:hover {
+    color: darkred;
+  }
